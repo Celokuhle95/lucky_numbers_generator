b0a90911db87f80a0d9b007e5d09a375b8a8bef4
celokuhle.myeza <celokuhle.myeza@supergrp.com>
2019-02-17 22:10:34 +0200

    back up before psybergate takes their laptops

--- winning_numbers_generator/src/com/cmbetting/generator/NumbersGenerator.java
+++ winning_numbers_generator/src/com/cmbetting/generator/NumbersGenerator.java
@@ -31,25 +31,48 @@ public class NumbersGenerator {
     }
 
     private static void playLotto() {
-        List<Integer> toPlayNumbers = getNumbers(LOTTO_NUM_BALLS, LOTTO_MAX, false);
+        List<Integer> toPlayNumbers = getNumbersUsingComplexWay(LOTTO_NUM_BALLS, LOTTO_MAX, false);
+//        List<Integer> toPlayNumbers = getNumbersUsingSimpleWay(LOTTO_NUM_BALLS, LOTTO_MAX, false);
         print("lotto", toPlayNumbers);
     }
 
     private static void playPowerball() {
-        List<Integer> toPlayNumbers = getNumbers(POWERBALL_NUM_BALLS, POWERBALL_OTHERS_MAX, false);
+        List<Integer> toPlayNumbers = getNumbersUsingComplexWay(POWERBALL_NUM_BALLS, POWERBALL_OTHERS_MAX, false);
+//        List<Integer> toPlayNumbers = getNumbersUsingSimpleWay(POWERBALL_NUM_BALLS, POWERBALL_OTHERS_MAX, false);
         print("powerball", toPlayNumbers);
 
-        List<Integer> theRealPB = getNumbers(1, POWERBALL_MAX, false);
+        List<Integer> theRealPB = getNumbersUsingComplexWay(1, POWERBALL_MAX, false);
+//        List<Integer> theRealPB = getNumbersUsingSimpleWay(1, POWERBALL_MAX, false);
         print("The real powerball", theRealPB);
 
     }
 
     private static void playPick3() {
-        List<Integer> toPlayNumbers = getNumbers(PICK3_NUM_BALLS, PICK3_MAX, true);
+        List<Integer> toPlayNumbers = getNumbersUsingComplexWay(PICK3_NUM_BALLS, PICK3_MAX, true);
+//        List<Integer> toPlayNumbers = getNumbersUsingSimpleWay(PICK3_NUM_BALLS, PICK3_MAX, true);
         print("pick3", toPlayNumbers);
     }
 
-    private static List<Integer> getNumbers(int numberOfBalls, int maxBall, boolean allowDuplicates) {
+    private static List<Integer> getNumbersUsingComplexWay(int numberOfBalls, int maxBall, boolean allowDuplicates) {
+        List<List<Integer>> listOfList = getListOfListNumbers(numberOfBalls, maxBall, allowDuplicates);
+        while (listOfList.size() > 1) {
+            Collections.shuffle(listOfList);
+            listOfList = eliminateFirstForListOfList(listOfList);
+        }
+        return listOfList.get(0);
+    }
+
+    private static List<List<Integer>> getListOfListNumbers(int numberOfBalls, int maxBall, boolean allowDuplicates) {
+        List<List<Integer>> toPlayListOfList = new ArrayList<>();
+
+        for (int i = 0; i < TOT_RAND_NUMS_TO_CHOOSE_FROM; i++) {
+            List<Integer> possibleNums = getNumbersUsingSimpleWay(numberOfBalls, maxBall, allowDuplicates);
+            toPlayListOfList.add(possibleNums);
+        }
+        return toPlayListOfList;
+    }
+
+    private static List<Integer> getNumbersUsingSimpleWay(int numberOfBalls, int maxBall, boolean allowDuplicates) {
         List<Integer> toPlayNumbers = new ArrayList<>();
 
         for (int i = 0; i < numberOfBalls; i++) {
@@ -78,6 +101,10 @@ public class NumbersGenerator {
     }
 
     private static List<Integer> eliminateFirst(List<Integer> numbers) {
+        return numbers.stream().skip(1).collect(Collectors.toList());
+    }
+
+    private static List<List<Integer>> eliminateFirstForListOfList(List<List<Integer>> numbers) {
         return numbers.stream().skip(1).collect(Collectors.toList());
     }
 
